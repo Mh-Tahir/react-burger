5342fb528f12286cc229792ab74e7efc71d68303
Mh-Tahir <tahirmr@mail.ru>
2021-07-29 16:04:06 +0300

    sprint1/step1 refactor

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -8,7 +8,7 @@ const BurgerConstructor = ({ data }) => {
     <section className={styles.container}>
       <div className={styles.locked}>
         <ul className={styles.list}>
-          <li className={styles.element + " mb-4 mr-2 ml-4 pl-8"} key={data[0]._id}>
+          <li className={styles.element + " mb-4 mr-2 ml-4 pl-8"}>
             <ConstructorElement
               type="top"
               isLocked={true}
@@ -29,7 +29,7 @@ const BurgerConstructor = ({ data }) => {
               )}
             </ul>
           </div>
-          <li className={styles.element + " mr-2 ml-4 pl-8"} key={data[0]._id}>
+          <li className={styles.element + " mr-2 ml-4 pl-8"}>
             <ConstructorElement
               type="bottom"
               isLocked={true}
@@ -58,18 +58,18 @@ const BurgerConstructor = ({ data }) => {
 BurgerConstructor.propTypes = {
   data: PropTypes.arrayOf(
     PropTypes.shape({
-      _id: PropTypes.string,
-      name: PropTypes.string,
-      type: PropTypes.string,
-      proteins: PropTypes.number,
-      fat: PropTypes.number,
-      carbohydrates: PropTypes.number,
-      calories: PropTypes.number,
-      price: PropTypes.number,
-      image: PropTypes.string,
-      image_mobile: PropTypes.string,
-      image_large: PropTypes.string,
-      __v: PropTypes.number,
+      _id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      proteins: PropTypes.number.isRequired,
+      fat: PropTypes.number.isRequired,
+      carbohydrates: PropTypes.number.isRequired,
+      calories: PropTypes.number.isRequired,
+      price: PropTypes.number.isRequired,
+      image: PropTypes.string.isRequired,
+      image_mobile: PropTypes.string.isRequired,
+      image_large: PropTypes.string.isRequired,
+      __v: PropTypes.number.isRequired,
     })
   ),
 };
